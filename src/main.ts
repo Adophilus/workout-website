61c901ee32151bcb2541e8e025529964ec32d21a
Adophilus <uchenna19of@gmail.com>
2023-09-01 15:46:24 +0100

    added some transitions

--- src/main.ts
+++ src/main.ts
@@ -4,39 +4,51 @@ import Alpine from "alpinejs";
 Alpine.store("exercises", [
   {
     title: "Treadmill",
-    image: "/public/exercise-treadmill.png",
+    image: "/exercise-treadmill.png",
     averageCalories: 250,
-    duration: "58:24",
+    duration: "21:18",
+    link:
+      "https://www.youtube.com/watch?v=tBNqEwcvYjU&pp=ygUSdHJlYWRtaWxsIGV4ZXJjaXNl",
   },
   {
     title: "Stretching",
-    image: "/public/exercise-stretching.png",
+    image: "/exercise-stretching.png",
     averageCalories: 250,
-    duration: "58:24",
+    duration: "15:32",
+    link:
+      "https://www.youtube.com/watch?v=7_Gmj7awnWY&pp=ygUTc3RyZXRjaGluZyBleGVyY2lzZQ%3D%3D",
   },
   {
     title: "Yoga",
-    image: "/public/exercise-yoga.png",
+    image: "/exercise-yoga.png",
     averageCalories: 250,
-    duration: "58:24",
+    duration: "25:37",
+    link:
+      "https://www.youtube.com/watch?v=Eml2xnoLpYE&pp=ygUNeW9nYSBleGVyY2lzZQ%3D%3D",
   },
   {
     title: "Running",
-    image: "/public/exercise-running.png",
+    image: "/exercise-running.png",
     averageCalories: 250,
-    duration: "58:24",
+    duration: "29:45",
+    link:
+      "https://www.youtube.com/watch?v=b7G4lkryqVM&pp=ygUQcnVubmluZyBleGVyY2lzZQ%3D%3D",
   },
   {
     title: "Lifting",
-    image: "/public/exercise-lifting.png",
+    image: "/exercise-lifting.png",
     averageCalories: 250,
-    duration: "58:24",
+    duration: "10:26",
+    link:
+      "https://www.youtube.com/watch?v=HWO2W5c9m7o&pp=ygUQbGlmdGluZyBleGVyY2lzZQ%3D%3D",
   },
   {
     title: "Push Ups",
-    image: "/public/exercise-pushups.png",
+    image: "/exercise-pushups.png",
     averageCalories: 250,
-    duration: "58:24",
+    duration: "11:55",
+    link:
+      "https://www.youtube.com/watch?v=Jf5_PJCFs-g&pp=ygUQcHVzaCB1cCBleGVyY2lzZQ%3D%3D",
   },
 ]);
 
@@ -45,19 +57,19 @@ Alpine.store("features", [
     title: "Personalization",
     description:
       "We create a workout program that is designed specifically for your body type, fitness level, and goals. No more wasting time on generic routines that don't deliver results.",
-    image: "/public/yoga-woman.jpg",
+    image: "/yoga-woman.jpg",
   },
   {
     title: "Expert Guidance",
     description:
       "Our team of experienced trainers and fitness experts will be with you throughout your journey. They'll provide continuous support, motivation, and expertise to ensure your success.",
-    image: "/public/trainer-and-student.jpg",
+    image: "/trainer-and-student.jpg",
   },
   {
     title: "Results-Driven",
     description:
       "Our goal is your success. Whether you want to shed pounds, build muscle, or simply improve your overall health, we'll help you achieve the results you've always dreamed of.",
-    image: "/public/man-dumbbells.jpg",
+    image: "/man-dumbbells.jpg",
   },
 ]);
 
@@ -81,6 +93,7 @@ Alpine.store("workouts", [
   {
     name: "Burn Calories",
     videos: 30,
+    link: "<BS>"
   },
 ]);
 
