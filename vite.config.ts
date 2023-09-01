fbb5ef26a1da7b56da63289a6d28f0833871ac36
Adophilus <uchenna19of@gmail.com>
2023-09-01 16:19:13 +0100

    updated github workflow file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig, loadEnv } from 'vite'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: env.base
+    base: "./"
   }
 })
