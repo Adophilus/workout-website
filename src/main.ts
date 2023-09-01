2d739987af56885c467c0672eb5783081ef089d8
Adophilus <uchenna19of@gmail.com>
2023-09-01 12:39:06 +0100

    updated the index page

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,43 @@
-import './globals.css'
-import Alpine from "alpinejs"
+import "./globals.css";
+import Alpine from "alpinejs";
 
 Alpine.store("exercises", [
   {
     title: "Treadmill",
     image: "/public/exercise-treadmill.png",
     averageCalories: 250,
-    duration: "58:24"
-  }
-])
+    duration: "58:24",
+  },
+  {
+    title: "Stretching",
+    image: "/public/exercise-stretching.png",
+    averageCalories: 250,
+    duration: "58:24",
+  },
+  {
+    title: "Yoga",
+    image: "/public/exercise-yoga.png",
+    averageCalories: 250,
+    duration: "58:24",
+  },
+  {
+    title: "Running",
+    image: "/public/exercise-running.png",
+    averageCalories: 250,
+    duration: "58:24",
+  },
+  {
+    title: "Lifting",
+    image: "/public/exercise-lifting.png",
+    averageCalories: 250,
+    duration: "58:24",
+  },
+  {
+    title: "Push Ups",
+    image: "/public/exercise-pushups.png",
+    averageCalories: 250,
+    duration: "58:24",
+  },
+]);
 
-Alpine.start()
+Alpine.start();
