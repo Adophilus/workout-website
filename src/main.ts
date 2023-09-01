8176e941d70b302b45712782aab3dcfeb2c08c3f
Adophilus <uchenna19of@gmail.com>
2023-09-01 08:56:18 +0100

    working on exercises section

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './globals.css'
+import Alpine from "alpinejs"
+
+Alpine.store("exercises", [
+  {
+    title: "Treadmill",
+    image: "/public/exercise-treadmill.png",
+    averageCalories: 250,
+    duration: "58:24"
+  }
+])
+
+Alpine.start()
